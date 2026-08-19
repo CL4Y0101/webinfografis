67dc71aacd0ab9741e603be96cb655bb2e4452dc
CL4Y0101 <aditya.fadni@gmail.com>
2026-08-20 00:00:46 +0700

    try again

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.8.155"],
   output: "standalone",
+  staticPageGenerationTimeout: 300,
 };
 
 export default nextConfig;
